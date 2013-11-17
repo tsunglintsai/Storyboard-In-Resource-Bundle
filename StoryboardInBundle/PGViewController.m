c7c6c90624e0c9515fcdebf27a6da4266dd3cc51
Henry Tsai <tsunglin.tsai@gmail.com>
2013-11-17 14:54:04 -0800

    Added all files

--- StoryboardInBundle/PGViewController.m
+++ StoryboardInBundle/PGViewController.m
@@ -20,24 +20,13 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 - (IBAction)buttonClicked:(id)sender {
     
     //NSBundle *resourceBundle = [NSBundle mainBundle];
     //NSBundle *resourceBundle = [NSBundle bundleWithIdentifier:@"bundlefilename.bundle"];
     NSBundle *resourceBundle = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"bundlefilename" ofType:@"bundle"]];
-    
-    NSBundle *settingBundle = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"Settings" ofType:@"bundle"]];
-    
     [resourceBundle load];
-    
-    
     NSLog(@"resourceBundle:%@",resourceBundle);
-    NSLog(@"settingBundle:%@",settingBundle);
     
     UIStoryboard *sb = [UIStoryboard storyboardWithName:@"StoryboardA" bundle:resourceBundle];
     UIViewController *vc = [sb instantiateViewControllerWithIdentifier:@"myViewController"];
