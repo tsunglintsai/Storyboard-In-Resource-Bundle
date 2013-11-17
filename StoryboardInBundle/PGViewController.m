d867cefcb5ec6aa197a2c6b6c7654c3d6dd5a4ed
Henry Tsai <tsunglin.tsai@gmail.com>
2013-11-17 14:53:46 -0800

    Added all files

--- StoryboardInBundle/PGViewController.m
+++ StoryboardInBundle/PGViewController.m
@@ -25,5 +25,24 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)buttonClicked:(id)sender {
+    
+    //NSBundle *resourceBundle = [NSBundle mainBundle];
+    //NSBundle *resourceBundle = [NSBundle bundleWithIdentifier:@"bundlefilename.bundle"];
+    NSBundle *resourceBundle = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"bundlefilename" ofType:@"bundle"]];
+    
+    NSBundle *settingBundle = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"Settings" ofType:@"bundle"]];
+    
+    [resourceBundle load];
+    
+    
+    NSLog(@"resourceBundle:%@",resourceBundle);
+    NSLog(@"settingBundle:%@",settingBundle);
+    
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"StoryboardA" bundle:resourceBundle];
+    UIViewController *vc = [sb instantiateViewControllerWithIdentifier:@"myViewController"];
+    vc.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    [self presentViewController:vc animated:YES completion:NULL];
+}
 
 @end
